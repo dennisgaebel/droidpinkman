1a0e9162238cf24780c8f71fdc2ad01710b2204e
Dennis Gaebel <dennisgaebel@gmail.com>
2015-08-06 23:27:18 -0400

    proper copy call for CNAME

--- gulpfile.js
+++ gulpfile.js
@@ -313,7 +313,7 @@ gulp.task('copy', ['usemin'], function() {
     gulp.src([
         'webhook.php',
         path.site + '/*.{ico,png,txt}',
-        path.site + '/{CNAME}',
+        path.site + '/CNAME',
       ]).pipe(gulp.dest(path.dist))
   );
 });
